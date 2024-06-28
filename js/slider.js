862f40d091a98e13041dd475cb0660ac1f53f748
Adberryweb <145831258+Adberryweb@users.noreply.github.com>
2024-06-28 12:56:02 -0600

    script.js

--- js/slider.js
+++ js/slider.js
@@ -1,31 +1,16 @@
-(function(){
-    
-    const sliders = [...document.querySelectorAll('.testimony__body')];
-    const buttonNext = document.querySelector('#next');
-    const buttonBefore = document.querySelector('#before');
-    let value;   
+document.addEventListener("DOMContentLoaded", function() {
+    const heartContainer = document.querySelector('.heart-container');
+    const createHeart = () => {
+        const heart = document.createElement('div');
+        heart.classList.add('heart');
+        heart.style.left = `${Math.random() * 100}%`;
+        heart.style.animationDuration = `${Math.random() * 3 + 2}s`; // Duración entre 2 y 5 segundos
+        heartContainer.appendChild(heart);
 
-    buttonNext.addEventListener('click', ()=>{
-        changePosition(1);
-    });
-
-    buttonBefore.addEventListener('click', ()=>{
-        changePosition(-1);
-    });
-
-    const changePosition = (add)=>{
-        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
-        value = Number(currentTestimony);
-        value+= add;
-
-
-        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
-        if(value === sliders.length+1 || value === 0){
-            value = value === 0 ? sliders.length  : 1;
-        }
-
-        sliders[value-1].classList.add('testimony__body--show');
-
+        setTimeout(() => {
+            heart.remove();
+        }, 5000); // Remueve el corazón después de 5 segundos
     }
 
-})();
+    setInterval(createHeart, 300);
+});
